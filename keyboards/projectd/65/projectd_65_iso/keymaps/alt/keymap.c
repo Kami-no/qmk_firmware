69c286cafb3ec84848b9c54130cda9f47ad0ed4d
Dmitry Zinin <admin@kami-no.ru>
2026-03-03 11:53:48 +0000

    fix RGB_A

--- keyboards/projectd/65/projectd_65_iso/keymaps/alt/keymap.c
+++ keyboards/projectd/65/projectd_65_iso/keymaps/alt/keymap.c
@@ -19,6 +19,7 @@
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 enum my_keycodes {
+    RGB_A,
     RGB_R = QK_USER,
     RGB_G,
     RGB_B,
